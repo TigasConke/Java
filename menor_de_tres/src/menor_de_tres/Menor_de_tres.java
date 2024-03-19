ffd34e8d9eaec17aa2d84ef6397d8ab19f504e58
TigasMoro <122630395+TigasMoro@users.noreply.github.com>
2024-03-19 16:38:04 -0300

    Update Menor_de_tres.java

--- menor_de_tres/src/menor_de_tres/Menor_de_tres.java
+++ menor_de_tres/src/menor_de_tres/Menor_de_tres.java
@@ -1,5 +1,3 @@
-package menor_de_tres;
-
 //Fazer um programa para ler três números inteiros. Em seguida, mostrar qual o menor dentre os três
 //números lidos. Em caso de empate, mostrar apenas uma vez. 
 

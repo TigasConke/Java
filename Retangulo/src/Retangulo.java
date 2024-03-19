4ded8487418e220ae43719e22d6ec4ebb88e91aa
TigasMoro <122630395+TigasMoro@users.noreply.github.com>
2024-03-19 16:13:21 -0300

    Update Retangulo.java

--- Retangulo/src/Retangulo.java
+++ Retangulo/src/Retangulo.java
@@ -1,3 +1,5 @@
+//Fazer um programa para ler as medidas da base e altura de um retângulo. Em seguida, mostrar o valor
+//da área, perímetro e diagonal deste retângulo.
 import java.util.Locale;
 import java.util.Scanner;
 import java.lang.Math;

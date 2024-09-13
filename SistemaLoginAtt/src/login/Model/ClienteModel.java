046931534d5eb3ec95759a6c5c914cba7b683943
Tiago de Souza Moro Conke <122630395+TigasConke@users.noreply.github.com>
2024-09-13 19:26:12 -0300

    Update ClienteModel.java

--- SistemaLoginAtt/src/login/Model/ClienteModel.java
+++ SistemaLoginAtt/src/login/Model/ClienteModel.java
@@ -3,6 +3,7 @@ package login.Model;
 import java.util.Date;
 
 public class ClienteModel {
+    
     private int codigo;
     private String nome;
     private String cpf;
@@ -10,71 +11,103 @@ public class ClienteModel {
     private String endereco;
     private Date dtNascimento;
     
-    // Construtor com todos os parâmetros
     public ClienteModel(int codigo, String nome, String cpf, String email, String endereco, Date dtNascimento) {
+        
         this.codigo = codigo;
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
         this.endereco = endereco;
         this.dtNascimento = dtNascimento;
+        
     }
 
-    // Construtor sem o código (para uso geral)
     public ClienteModel(String nome, String cpf, String email, String endereco, Date dtNascimento) {
+        
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
         this.endereco = endereco;
         this.dtNascimento = dtNascimento;
+        
     }
     
-    // Getters e Setters
     public int getCodigo() {
+        
         return codigo;
+        
     }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    
     }
 
     public String getCpf() {
+        
         return cpf;
+        
     }
 
+    public void setCodigo(int codigo) {
+        
+        this.codigo = codigo;
+        
+    }
+
+    public String getNome() {
+        
+        return nome;
+        
+    }
+
+    public void setNome(String nome) {
+        
+        this.nome = nome;
+        
+
+
     public void setCpf(String cpf) {
+        
         this.cpf = cpf;
+        
     }
 
     public String getEmail() {
+        
         return email;
+        
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    
     public String getEndereco() {
+        
         return endereco;
+        
     }
+    
+    public void setEmail(String email) {
+        
+        this.email = email;
+        
+    }
+
+
 
     public void setEndereco(String endereco) {
+        
         this.endereco = endereco;
+        
     }
-
-    public Date getDtNascimento() {
-        return dtNascimento;
-    }
-
-    public void setDtNascimento(Date dtNascimento) {
+    
+ public void setDtNascimento(Date dtNascimento) {
+        
         this.dtNascimento = dtNascimento;
+        
     }
+    
+    public Date getDtNascimento() {
+        
+        return dtNascimento;
+        
+    }
+
+   
+    
 }

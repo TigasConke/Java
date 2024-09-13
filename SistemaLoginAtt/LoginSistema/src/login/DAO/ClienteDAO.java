0a181cea907eecd2906cca6167c4b35d14c2b09c
Tiago de Souza Moro Conke <122630395+TigasConke@users.noreply.github.com>
2024-09-13 19:17:41 -0300

    Update ClienteDAO.java

--- SistemaLoginAtt/LoginSistema/src/login/DAO/ClienteDAO.java
+++ SistemaLoginAtt/LoginSistema/src/login/DAO/ClienteDAO.java
@@ -11,22 +11,23 @@ public class ClienteDAO {
         this.conexaoDAO = new ConexaoDAO();
     }
 
-    // Método para cadastrar um novo cliente
     public String cadastrarCliente(String nome, String cpf, String email, String endereco) {
+        
         String sql = "INSERT INTO cliente (nome, cpf, email, endereco) VALUES (?, ?, ?, ?)";
         String resultado = "Erro ao cadastrar cliente!";
 
         try (
+            
             Connection conn = conexaoDAO.conectarBD();
-            PreparedStatement stmt = conn.prepareStatement(sql)
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            
         ) {
-            // Define os parâmetros da consulta
+           
             stmt.setString(1, nome);
             stmt.setString(2, cpf);
             stmt.setString(3, email);
             stmt.setString(4, endereco);
-
-            // Executa a inserção
+            
             stmt.executeUpdate();
             resultado = "Cliente cadastrado com sucesso!";
         } catch (SQLException e) {

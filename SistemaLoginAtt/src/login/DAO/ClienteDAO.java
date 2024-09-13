5d894cae1606f976a2a9a4a6474cf3c921982458
Tiago de Souza Moro Conke <122630395+TigasConke@users.noreply.github.com>
2024-09-13 19:22:26 -0300

    Update ClienteDAO.java

--- SistemaLoginAtt/src/login/DAO/ClienteDAO.java
+++ SistemaLoginAtt/src/login/DAO/ClienteDAO.java
@@ -13,27 +13,34 @@ public class ClienteDAO {
 
     public ArrayList<ClienteModel> buscarClientesPorNome(String nome) {
     String sql;
+        
     if (nome == null || nome.trim().isEmpty()) {
-        // Se o nome for vazio ou nulo, busca todos os clientes
+        
         sql = "SELECT * FROM cliente";
+        
     } else {
-        // Busca clientes cujo nome contenha a string fornecida
+     
         sql = "SELECT * FROM cliente WHERE nome LIKE ?";
+        
     }
     
     ArrayList<ClienteModel> clientes = new ArrayList<>();
 
     try {
+        
         Connection conn = ConexaoDAO.conectarBD();
         PreparedStatement pstmt = conn.prepareStatement(sql);
 
         if (nome != null && !nome.trim().isEmpty()) {
+            
             pstmt.setString(1, "%" + nome + "%");
+            
         }
 
         ResultSet rs = pstmt.executeQuery();
 
         while (rs.next()) {
+            
             ClienteModel cliente = new ClienteModel(
                 rs.getInt("codigo"), 
                 rs.getString("nome"),
@@ -41,24 +48,30 @@ public class ClienteDAO {
                 rs.getString("email"),
                 rs.getString("endereco"),
                 rs.getDate("dtnascimento")
+                
             );
             clientes.add(cliente);
         }
 
     } catch (SQLException e) {
+        
         e.printStackTrace();
         JOptionPane.showMessageDialog(null, "Erro ao buscar clientes: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        
     }
 
     return clientes;
+        
 }
 
 
     public String cadastrarCliente(ClienteModel cliente) {
+        
         String sql = "INSERT INTO cliente (nome, cpf, email, endereco, dtNascimento) VALUES (?, ?, ?, ?, ?)";
         String resultado = "Erro ao cadastrar cliente!";
 
         try {
+            
             Connection conn = ConexaoDAO.conectarBD();
             PreparedStatement pstmt = conn.prepareStatement(sql);
 
@@ -69,16 +82,24 @@ public class ClienteDAO {
             pstmt.setDate(5, new Date(cliente.getDtNascimento().getTime()));
 
             int rowsAffected = pstmt.executeUpdate();
+            
             if (rowsAffected > 0) {
+
                 return "Usuário cadastrado com sucesso!";
+                
             } else {
+                
                 return "Usuário não cadastrado!";
+                
             }
 
         } catch (SQLException e) {
+            
             System.err.println("Erro ao cadastrar o cliente: " + e.getMessage());
+            
         }
 
         return resultado;
+        
     }
 }
